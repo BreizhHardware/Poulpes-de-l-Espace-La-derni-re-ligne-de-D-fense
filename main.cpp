960f5c7a8790e439180812f5bf3ab34823995bab
Félix MARQUET <fpmovie4@gmail.com>
2024-04-17 10:38:45 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
+    QApplication::setApplicationName("Poules de l'espace: La dernière ligne de défense");
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
     button.show();
